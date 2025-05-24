7fafea75f4ed640a5888e783c3e31dc92207c013
Andikapratama1029 <168614929+Andikapratama1029@users.noreply.github.com>
2025-05-24 18:07:17 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
+global.creator = 'DREAM STALKER'// yourname
+global.MONGO_DB_URI =  "mongodb+srv://andikapratama:47EG1RzPPd1RLh8b@cluster0.pyppely.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "shafe//2025" //isi apa aja bebas
 global.your_email = "" //email
 global.email_password = "" //application password email
-global.limitCount = 10000
+global.limitCount = 100
 global.YUOR_PORT = 8000
 global.loghandler = {
 	noapikey:{
